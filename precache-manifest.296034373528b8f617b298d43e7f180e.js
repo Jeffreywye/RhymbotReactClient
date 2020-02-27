ea21a519d261debd2718389c164c15bb803b5b29
Jeffreywye <jeffreywye7@gmail.com>
2020-02-27 03:33:05 -0500

    Updates

---
+++ precache-manifest.296034373528b8f617b298d43e7f180e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f76019bf3234f9806676dff8b0f6411",
+    "url": "/RhymebotReactClient/index.html"
+  },
+  {
+    "revision": "cb1b78cf27fc7fc6fc39",
+    "url": "/RhymebotReactClient/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "47ceeec33d033a66f584",
+    "url": "/RhymebotReactClient/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb1b78cf27fc7fc6fc39",
+    "url": "/RhymebotReactClient/static/js/2.fa0285b4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RhymebotReactClient/static/js/2.fa0285b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ceeec33d033a66f584",
+    "url": "/RhymebotReactClient/static/js/main.168c2af7.chunk.js"
+  },
+  {
+    "revision": "161b1d86019bee76a8d8",
+    "url": "/RhymebotReactClient/static/js/runtime-main.95f7111c.js"
+  }
+]);
